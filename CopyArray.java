50b37ecc8413ba46abbd3e1b204f4a2fdb71fa20
Kalpana. M <kalpanaramakrishna25@gmail.com>
2026-07-06 20:12:11 +0530

    Create CopyArray.java

---
+++ CopyArray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int[] copy = new int[n];
+
+        System.out.println("Enter the array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            copy[i] = arr[i];
+        }
+
+        System.out.println("Copied Array:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(copy[i] + " ");
+        }
+
+        sc.close();
+    }
+}
